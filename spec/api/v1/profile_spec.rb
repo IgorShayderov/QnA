8f8aef761803ed3fae7d7e4bafce305e56cfc285
IgorShayderov <flashmob123@gmail.com>
2020-08-15 00:54:11 +0200

    add profiles api

--- spec/api/v1/profile_spec.rb
+++ spec/api/v1/profile_spec.rb
@@ -4,6 +4,8 @@ describe 'Profiles API', type: :request do
   let(:headers) do
     { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
   end
+  let!(:me) { create(:user) }
+  let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
   describe 'GET /api/v1/profiles/me' do
     let(:api_path) { '/api/v1/profiles/me' }
@@ -13,9 +15,6 @@ describe 'Profiles API', type: :request do
     end
 
     context 'authorized' do
-      let(:me) { create(:user) }
-      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-
       before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
       it 'returns 200 status' do
@@ -33,6 +32,45 @@ describe 'Profiles API', type: :request do
           expect(json).to_not have_key(attr)
         end
       end
-    end  
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+    let!(:profiles) { create_list(:user, 3) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields for each profile' do
+        profiles.each_with_index do |profile, index|
+          %w[id email admin created_at updated_at].each do |attr|
+            expect(json[index][attr]).to eq profile.send(attr).as_json
+          end
+        end
+      end
+
+      it 'does not returns private fields for each profile' do
+        profiles.each_with_index do |profile, index|
+          %w[password encrypted_password].each do |attr|
+            expect(json[index]).to_not have_key(attr)
+          end
+        end
+      end
+
+      it 'does not return me' do
+        profiles.each do |profile|
+          expect(profile['id']).to_not eq me.id
+        end
+      end
+    end
   end
 end
