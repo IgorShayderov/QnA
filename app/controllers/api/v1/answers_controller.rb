ceb48423de966e1303543aa57d2e0dbe96ecaef3
IgorShayderov <flashmob123@gmail.com>
2020-08-16 02:15:38 +0200

    question/answers create/update/destroy api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,13 +1,57 @@
 class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_answer, only: %i[show update destroy]
+  before_action :set_question, only: %i[index create]
   authorize_resource
 
   def index
-    @answers = Question.find(params[:question_id]).answers
+    @answers = @question.answers
     render json: @answers
   end
 
   def show
-    @answer = Answer.find(params[:id])
     render json: @answer
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      render json: @answer, status: :created
+    else
+      render json: { errors: @answer.errors }, status: :unproccessable_entitty
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      render json: @answer, status: :accepted
+    else
+      render json: { errors: @answer.errors }, status: :unproccessable_entitty
+    end
+  end
+
+  def destroy
+    if @answer.destroy
+      render json: {}, status: :ok
+    else
+      render json: { errors: @answer.errors }, status: :unproccessable_entitty
+    end
+  end
+
+  private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(
+      :body,
+      links_attributes: %i[id name url _destroy]
+    )
   end
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :set_question, only: %i[update destroy]
   authorize_resource
 
   def index
-    @questions = Question.all
+    @questions = Question.with_attached_files.all
     render json: @questions
   end
 
@@ -10,5 +11,45 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     @question = Question.with_attached_files.find(params[:id])
 
     render json: @question
+  end
+
+  def create
+    @question = current_resource_owner.questions.new(question_params)
+
+    if @question.save
+      render json: @question, status: :created
+    else
+      render json: { errors: @question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      render json: @question, status: :accepted
+    else
+      render json: { errors: @question.errors }, status: :unproccessable_entity
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      render json: {}, status: :ok
+    else
+      render json: { errors: @question.errors }, status: :uproccessable_entity
+    end
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(
+      :title,
+      :body,
+      links_attributes: %i[id name url _destroy]
+    )
   end
 end
