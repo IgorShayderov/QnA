d69cb98a7a1693406b1000aa634d7f27a755b145
IgorShayderov <flashmob123@gmail.com>
2020-08-22 00:30:24 +0200

    fixed tests

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe SubscriptionsController, type: :controller do
   let!(:user) { create(:user) }
+  let!(:question) { create(:question) }
+
   before { login(user) }
 
   describe 'POST #create' do
-    let!(:question) { create(:question) }
 
     it 'creates new subscription for question' do
       expect { post :create, params: { question_id: question.id } }.to change(Subscription, :count).by(1)
@@ -19,6 +20,16 @@ RSpec.describe SubscriptionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    let!(:subscription) { create(:subscription, question: question) }
 
+    it 'deletes subscription' do
+      expect { delete :destroy, params: { id: subscription.id } }.to change(Subscription, :count).by(-1)
+    end
+
+    it 'redirects to question' do
+      delete :destroy, params: { id: subscription.id }
+
+      expect(response).to redirect_to question
+    end
   end
 end
--- spec/services/subscription_service_spec.rb
+++ spec/services/subscription_service_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe SubscriptionService do
   let!(:subscribed_users) { create_list(:user, 3) }
   let(:unsubscribed_users) { create_list(:user, 3) }
   let!(:question) { create(:question) }
-  subject { SubscriptionService.new }
 
   before do
     subscribed_users.each do |user|
       create(:subscription, question: question, user: user)
     end
   end
+
   let!(:answer) { create(:answer, question: question) }
 
   it 'sends new asnwer notification to all subscribed users' do
@@ -20,13 +20,13 @@ RSpec.describe SubscriptionService do
         .with(user, answer)
         .and_call_original
     end
+
+    subject.notificate(answer)
   end
 
   it 'does not sends new asnwer notification to unsubscribed users' do
-    unsubscribed_users.each do |user|
-      expect(NewAnswerNotificationMailer)
-        .to_not receive(:send_notification)
-        .with(user, answer)
-    end
+    expect(NewAnswerNotificationMailer).to receive(:send_notification).exactly(3).times.and_call_original
+
+    subject.notificate(answer)
   end
 end
