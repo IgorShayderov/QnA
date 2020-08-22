db2561d2eb9030d65210a80436b3df1feb391ce4
IgorShayderov <flashmob123@gmail.com>
2020-08-22 14:07:57 +0200

    hot fix

--- app/services/subscription_service.rb
+++ app/services/subscription_service.rb
@@ -1,6 +1,6 @@
 class SubscriptionService
   def notificate(answer)
-    User.joins(:subscriptions).where(subscriptions: { question_id: answer.question.id }).find_each(batch_size: 500) do |user|
+    answer.question.subscribers.find_each(batch_size: 500) do |user|
       NewAnswerNotificationMailer.send_notification(user, answer).deliver_later
     end
   end
