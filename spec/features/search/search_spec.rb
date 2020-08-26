acadadce739a00e9b572c34f3b438ff15b2ea5f3
IgorShayderov <flashmob123@gmail.com>
2020-08-27 00:10:44 +0200

    repaired sphinx tests

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -8,21 +8,40 @@ feature 'User can search records through search bar', "
 let!(:question) { create(:question, body: 'extraterrestial') }
 
   describe 'search', sphinx: true, js: true do
-    it 'existing record' do
-      visit root_path
+    context 'existing record' do
+      it 'via global search' do
+        visit root_path
 
-      fill_in 'context', with: 'extraterresial'
-      click_button 'Search'
-  
-      expect(page).to have_content 'Question'
-      expect(page).to have_content 'extraterrestial'
+        ThinkingSphinx::Test.run do
+          fill_in 'context', with: 'extraterrestial'
+          click_button 'Search'
+        end
+
+        expect(page).to have_content 'Question'
+        expect(page).to have_content 'extraterrestial'
+      end
+
+      it 'via question search' do
+        visit root_path
+        
+        ThinkingSphinx::Test.run do
+          fill_in 'context', with: 'extraterrestial'
+          select 'Questions', from: 'option'
+          click_button 'Search'
+        end
+
+        expect(page).to have_content 'Question'
+        expect(page).to have_content 'extraterrestial'
+      end
     end
   
     it 'non-existing record' do
       visit root_path
   
-      fill_in 'context', with: 'sophisticated'
-      click_button 'Search'
+      ThinkingSphinx::Test.run do
+        fill_in 'context', with: 'sophisticated'
+        click_button 'Search'
+      end
   
       expect(page).to_not have_content 'sophisticated'
     end
