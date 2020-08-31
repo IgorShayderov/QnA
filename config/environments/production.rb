880c742b9470d28fdbac4a414658020f040df4eb
IgorShayderov <flashmob123@gmail.com>
2020-08-31 12:51:46 +0200

    changed mailer authentication in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+    authentication: 'login',
     enable_starttls_auto: true
   }
 
